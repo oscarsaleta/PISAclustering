a3779e54f7308f3ae72317732b01c83e50bc5a0c
Oscar Saleta <oscar.saleta@gmail.com>
2016-01-21 22:39:40 +0100

    matrix with groups per each seed

--- KMeans/clusteringLibrary.R
+++ KMeans/clusteringLibrary.R
@@ -460,11 +460,11 @@ loopThroughSeeds <- function(a,countries,minSeed,maxSeed) {
     }
     if (fitness>bestFitness) {
       print(paste("New best seed found:",i))
-      groupmatrix[1,i] <- f$p
+      groupmatrix[1,i+1] <- f$p
       for (j in 1:nrow(a)) {
-        if (j %in% groups$g1)
+        if (sortedScores[j] %in% groups$g1)
           groupmatrix[j+1,i+1] <- 1
-        if (j %in% groups$g2)
+        if (sortedScores[j] %in% groups$g2)
           groupmatrix[j+1,i+1] <- 2
       }
       bestFitness <- fitness
